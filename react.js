b20d1b9984d0bc13badc7664b30be471b7a6e430
Yoshitaka Terazawa <locol6622@gmail.com>
2019-10-22 10:13:54 +0900

    refactor: change rules

--- react.js
+++ react.js
@@ -1,34 +1,27 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
-  extends: [
-    'airbnb',
-    'plugin:react/recommended',
-    'prettier',
-    'prettier/react'
-  ],
+  extends: ['airbnb', 'plugin:react/recommended', 'prettier', 'prettier/react'],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
   settings: {
     react: { version: 'detect' },
   },
-  plugins: [
-    'react',
-    'react-hooks'
-  ],
+  plugins: ['react', 'react-hooks'],
   rules: {
     'import/prefer-default-export': 0,
     'import/no-unresolved': 0,
     'react/jsx-filename-extension': [2, { extensions: ['.tsx'] }],
     'react/prop-types': 'off',
     'react/jsx-boolean-value': 2,
+    'react/jsx-props-no-spreading': 'off',
     'react/destructuring-assignment': ['error', 'never'],
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
-  }
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 }
