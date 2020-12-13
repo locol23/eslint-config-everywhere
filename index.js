39b24879673651b42b9f07f733ec454a0eb23d6b
Yoshitaka Terazawa <locol6622@gmail.com>
2020-12-13 18:24:09 +0900

    feat: update-eslint-rules

--- index.js
+++ index.js
@@ -5,5 +5,15 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['prettier'],
-  rules: {},
+  rules: {
+    semi: ['error', 'never'],
+    'comma-dangle': 0,
+    'operator-linebreak': 0,
+    'arrow-body-style': ['error', 'as-needed'],
+    'object-curly-newline': 0,
+    'class-methods-use-this': 0,
+    'max-len': 0,
+    'implicit-arrow-linebreak': 0,
+    'function-paren-newline': 0,
+  },
 }
--- react.js
+++ react.js
@@ -17,16 +17,17 @@ module.exports = {
   settings: {
     react: { version: 'detect' },
   },
-  plugins: ['react'],
   rules: {
     'import/prefer-default-export': 0,
     'import/no-unresolved': 0,
     'import/extensions': ['error', 'never', { json: 'always' }],
     'react/react-in-jsx-scope': 0,
+    'react/display-name': 0,
     'no-use-before-define': 0,
     'react/jsx-filename-extension': [2, { extensions: ['.tsx'] }],
     'react/prop-types': 'off',
     'react/jsx-boolean-value': 2,
     'react/jsx-props-no-spreading': 'off',
+    'react/no-danger': 0,
   },
 }
